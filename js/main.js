01c09f2be61b1af30eb00cc3f84385ee71f1e7f7
Dionathan Cordova <dionathan_bass@hotmail.com>
2017-01-14 22:58:16 -0200

    batalhar escolhendo mesmo  personagem

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
-
+    clique = false;
     //ocultar div escolha, colocado apenas para teste
     $(".escolha").hide()
+    $(".resultado").hide()
         //incio dos objetos	
     var Lutadores = function(nome, forca, velocidade, altura, cla, resistencia) {
         this.nome = nome;
@@ -10,18 +11,6 @@ $(document).ready(function() {
         this.altura = altura;
         this.cla = cla;
         this.resistencia = resistencia;
-
-        this.luta = function(lutador1, lutador2) {
-            if (lutador1 == lutador2) {
-                document.write("Essa luta não pode ocorrer entre lutadores iguais.")
-            }
-
-            if (lutador1.forca > lutador2.forca && lutador1.resistencia > lutador2.resistencia) {
-                document.write("O vencedor indubtavelmente é " + lutador1.nome);
-            } else {
-                document.write("O vencedor indubtavelmente é  " + lutador2.nome);
-            }
-        }
     }
 
     var naruto = new Lutadores("Naruto", 79, 89, 1.80, "Usumaki", 95)
@@ -35,44 +24,156 @@ $(document).ready(function() {
 
 
 
-    // lista de todos os ninjas em foto
-    var lutador2 = ["img/sasuke.png", "img/naruto.png", "img/sakura.png",
-        "img/kakashi.png", "img/shino.png", "img/neji.png", "img/orochimaro.png",
-        "img/shikamaru.png"
-    ]
-
     $("img").click(function() {
-        id = $(this).attr('src')
-        nome = $(this).attr("id");
-        switch (nome) {
-            case 'naruto':
-                obj = naruto;
-                break;
-            case 'sasuke':
-                obj = sasuke;
-                break;
-            case 'sakura':
-                obj = sakura;
-                break;
-            case 'kakashi':
-                obj = kakashi;
-                break;
-            case 'orochimaru':
-                obj = orochimaru;
-            case 'shikamaru':
-                obj = shikamaru;
-                break;
-            case 'shino':
-                obj = shino;
-                break;
-            case 'neji':
-                obj = neji;
-                break;
+        if (clique == false) {
+            clique = true;
 
+            id = $(this).attr('src')
+            nome = $(this).attr("id");
+            switch (nome) {
+                case 'naruto':
+                    obj = naruto;
+                    break;
+                case 'sasuke':
+                    obj = sasuke;
+                    break;
+                case 'sakura':
+                    obj = sakura;
+                    break;
+                case 'kakashi':
+                    obj = kakashi;
+                    break;
+                case 'orochimaru':
+                    obj = orochimaru;
+                case 'shikamaru':
+                    obj = shikamaru;
+                    break;
+                case 'shino':
+                    obj = shino;
+                    break;
+                case 'neji':
+                    obj = neji;
+                    break;
+
+            }
+            // campo do lutador 1 recebe imagem do ninja selecionado no click
+            $("#lutador1").attr("src", id);
+            $(".descricao_lutador1").html("<span style='color:#fff; font-size:30px'>Nome: " + obj.nome +
+                "<br>Resistencia: " + obj.resistencia + "<br>Cla: " + obj.cla + "<br>Força: " + obj.forca +
+                "<br>Altura: " + obj.altura + "</span>")
+
+
+            // lista de todos os ninjas em foto
+            var lutador2 = ["img/sasuke.png", "img/naruto.png", "img/sakura.png",
+                "img/kakashi.png", "img/shino.png", "img/neji.png", "img/orochimaro.png",
+                "img/shikamaru.png"
+            ]
+
+            // numero aleatorio para lutador 2
+            l2 = Math.floor((Math.random() * 7) + 1);
+            switch (l2) {
+                case 0:
+                    ninjaoponente = sasuke;
+                    break;
+                case 1:
+                    ninjaoponente = naruto;
+                    break;
+                case 2:
+                    ninjaoponente = sakura;
+                    break;
+                case 3:
+                    ninjaoponente = kakashi;
+                    break;
+                case 4:
+                    ninjaoponente = shino;
+                    break;
+                case 5:
+                    ninjaoponente = neji;
+                    break;
+                case 6:
+                    ninjaoponente = orochimaru;
+                    break;
+                case 7:
+                    ninjaoponente = shikamaru;
+                    break;
+            }
+
+            while (id === lutador2[l2]) {
+                // se lutado aleatorio for igual ao ja selecionado, gera um numero novo.
+                l2 = Math.floor((Math.random() * 7) + 1);
+                $("#lutador2").attr("src", lutador2[l2]);
+
+            }
+            // campo do lutado2 recebe a imagem com numero aleatorio
+            $("#lutador2").attr("src", lutador2[l2]);
+            $(".descricao_lutador2").html("<span style='color:#fff; font-size:30px'>Nome: " + ninjaoponente.nome +
+                "<br>Resistencia: " + ninjaoponente.resistencia + "<br>Cla: " + ninjaoponente.cla +
+                "<br>Força: " + ninjaoponente.forca + "<br>Altura: " + obj.altura + "</span>");;
+
+            // funcao de animacao da batalha;
+            status();
         }
+    });
+
+    function status() {
+        //$(".som").html("<audio src='sound/narutosound.mp3' autoplay='autoplay' loop='loop'>")
+        $(".escolha").show()
+        setTimeout(function() {
+            $(".statusDaLuta").html("<p style='color:#f00'>A batalha começou...</p>")
+            $(".gopes").attr('src', 'img/chidori.png')
+        }, 2000)
+
+        setTimeout(function() {
+            $(".statusDaLuta").html("<p style='color:#f00'>Que duelo incrível...</p>")
+            $(".gopes").attr('src', 'img/chidori2.png')
+        }, 3000)
+
+        setTimeout(function() {
+            $(".statusDaLuta").html("<p style='color:#f00'>Você não vai sair impune!</p>")
+            $(".gopes").attr('src', 'img/susano.png')
+        }, 4000)
+
+        setTimeout(function() {
+            $(".statusDaLuta").html("<p style='color:#f00'>É o que veremos!</p>")
+            vencedor()
+        }, 5000)
+
+    }
+
+    function vencedor() {
+        $(".escolha").hide();
+
+        numWinner = (Math.floor(Math.random() * 2) + 1)
+        if (numWinner == 1) {
+            $(".imgVencedor").attr("src", id)
+        } else if (numWinner == 2) {
+            $(".imgVencedor").attr("src", lutador2[l2])
+        }
+        /*
+        if(obj.forca > ninjaoponente.forca && obj.resistencia > ninjaoponente.resistencia){
+            $(".imgVencedor").attr("src", id)
+        }else if(ninjaoponente.forca > obj.forca && ninjaoponente.resistencia > obj.resistencia){
+            $(".imgVencedor").attr("src", lutador2[l2])
+        }else if (obj.velocidade > ninjaoponente.velocidade && obj.altura > ninjaoponente.altura){
+            $(".imgVencedor").attr("src", id)
+        }else if (ninjaoponente.velocidade > obj.velocidade && ninjaoponente.altura > obj.altura){
+            $(".imgVencedor").attr("src", lutador2[l2])
+        }
+        */
+        $(".resultado").show();
+
+    }
+
+    // Ao escolher lutar novamente com o mesmo ninja , a funcao again escolhera um novo oponente para a luta;
+    function again() {
+        // lista de todos os ninjas em foto
+        var lutador2 = ["img/sasuke.png", "img/naruto.png", "img/sakura.png",
+            "img/kakashi.png", "img/shino.png", "img/neji.png", "img/orochimaro.png",
+            "img/shikamaru.png"
+        ]
 
         // numero aleatorio para lutador 2
-        var l2 = Math.floor((Math.random() * 7) + 1);
+        l2 = Math.floor((Math.random() * 7) + 1);
         switch (l2) {
             case 0:
                 ninjaoponente = sasuke;
@@ -99,50 +200,32 @@ $(document).ready(function() {
                 ninjaoponente = shikamaru;
                 break;
         }
-        // campo do lutador 1 recebe imagem do ninja selecionado no click
-        $("#lutador1").attr("src", id);
-        $(".descricao_lutador1").html("<span style='color:#fff; font-size:30px'>Nome: " + obj.nome +
-            "<br>Resistencia: " + obj.resistencia + "<br>Cla: " + obj.cla + "<br>Força: " + obj.forca +
-            "<br>Altura: " + obj.altura + "</span>")
-
-
-        if (lutador2[l2] == id) {
+        while (id === lutador2[l2]) {
             // se lutado aleatorio for igual ao ja selecionado, gera um numero novo.
-            var l2 = Math.floor((Math.random() * 7) + 1);
+            l2 = Math.floor((Math.random() * 7) + 1);
             $("#lutador2").attr("src", lutador2[l2]);
 
         }
-        // campo do lutado2 recebe a imagem com numero aleatorio
         $("#lutador2").attr("src", lutador2[l2]);
         $(".descricao_lutador2").html("<span style='color:#fff; font-size:30px'>Nome: " + ninjaoponente.nome +
             "<br>Resistencia: " + ninjaoponente.resistencia + "<br>Cla: " + ninjaoponente.cla +
-            "<br>Força: " + ninjaoponente.forca + "<br>Altura: " + obj.altura + "</span>")
+            "<br>Força: " + ninjaoponente.forca + "<br>Altura: " + obj.altura + "</span>");;
+
+        // chama a funcao de animacao da batalha
         status();
-    });
 
-    function status() {
-        $(".som").html("<audio src='sound/narutosound.mp3' autoplay='autoplay' loop='loop'>")
-        $(".escolha").show()
-        setInterval(function() {
-            $(".statusDaLuta").html("<p style='color:#f00'>A batalha começou...</p>")
-            $(".gopes").attr('src', 'img/chidori.png')
-        }, 2000)
-
-        setInterval(function() {
-            $(".statusDaLuta").html("<p style='color:#f00'>Que duelo incrível...</p>")
-            $(".gopes").attr('src', 'img/chidori2.png')
-        }, 3000)
-
-        setInterval(function() {
-            $(".statusDaLuta").html("<p style='color:#f00'>Você não vai sair impune!</p>")
-            $(".gopes").attr('src', 'img/susano.png')
-        }, 4000)
-
-        setInterval(function() {
-            $(".statusDaLuta").html("<p style='color:#f00'>É o que veremos!</p>")
-        }, 5000)
     }
 
+    $(".opcaoSair").click(function() {
+        $(".resultado").hide();
+        clique = false;
+    })
+    $(".opcaoMesmo").click(function() {
+        $(".resultado").hide();
+        again();
+        //$(".escolha").show();
+        clique = false;
+    })
 
 });
 //status de luta - iniciada em 14-01-2017 as 01:57 am
